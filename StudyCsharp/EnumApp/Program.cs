dfa43344d6f4d095a473f3f78de57bee41994fb6
yujin <dywjd2341@naver.com>
2020-07-06 09:50:12 +0900

    20/07/06

---
+++ StudyCsharp/EnumApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumApp
+{
+    class Program
+    {
+        enum DialogResult { YES, NO, CANCEL, CONFIRM, OK }
+        static void Main(string[] args)
+        {
+            WriteLine((int)DialogResult.YES);
+            WriteLine((int)DialogResult.NO);
+            WriteLine((int)DialogResult.CANCEL);
+            WriteLine((int)DialogResult.CONFIRM);
+            WriteLine((int)DialogResult.OK);
+        }
+    }
+}
